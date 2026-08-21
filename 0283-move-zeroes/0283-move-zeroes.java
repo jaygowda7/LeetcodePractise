1a800c9a5aac9f1caa210dc977177a1f48dccaff
Jayanth Gowda C <121957270+jaygowda7@users.noreply.github.com>
2026-08-21 23:18:24 +0530

    Time: 2 ms (91.97%), Space: 47.7 MB (57.71%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -2,7 +2,6 @@ class Solution {
     public void moveZeroes(int[] nums) {
         int i=0;
         for(int j=0;j<nums.length;j++){
-
             if(nums[j]!=0){
                 int temp=nums[j];
                 nums[j]=nums[i];
