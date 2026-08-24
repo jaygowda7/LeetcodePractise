805e1310e9915fe8f2e3b2b74b97cc280b90b389
Jayanth Gowda C <121957270+jaygowda7@users.noreply.github.com>
2026-08-24 22:22:45 +0530

    Time: 4 ms (66.81%), Space: 91.8 MB (43.4%) - LeetHub

--- 1872-stone-game-viii/1872-stone-game-viii.java
+++ 1872-stone-game-viii/1872-stone-game-viii.java
@@ -1,9 +1,8 @@
 class Solution {
     public int stoneGameVIII(int[] stones) {
 
-        int n =stones.length;
-
-        int []pre=new int[n];
+        int n=stones.length;
+        int[] pre=new int[n];
         pre[0]=stones[0];
         for(int i=1;i<n;i++){
             pre[i]=pre[i-1]+stones[i];
