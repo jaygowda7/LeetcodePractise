2c1f4fecfa0fd6583dc09572bcf3734ef9b357e1
Jayanth Gowda C <121957270+jaygowda7@users.noreply.github.com>
2026-08-24 22:19:55 +0530

    Time: 3 ms (100%), Space: 92 MB (42.55%) - LeetHub

--- 1872-stone-game-viii/1872-stone-game-viii.java
+++ 1872-stone-game-viii/1872-stone-game-viii.java
@@ -1,24 +1,17 @@
 class Solution {
     public int stoneGameVIII(int[] stones) {
 
-        int n = stones.length;
+        int n =stones.length;
 
-        // Build prefix sums
-        int[] prefix = new int[n];
-        prefix[0] = stones[0];
-
-        for (int i = 1; i < n; i++) {
-            prefix[i] = prefix[i - 1] + stones[i];
+        int []pre=new int[n];
+        pre[0]=stones[0];
+        for(int i=1;i<n;i++){
+            pre[i]=pre[i-1]+stones[i];
         }
-
-        // dp = best score difference
-        // starting from prefix[i + 1]
-        int dp = prefix[n - 1];
-
-        for (int i = n - 2; i >= 1; i--) {
-            dp = Math.max(dp, prefix[i] - dp);
+        int dp=pre[n-1];
+        for(int i=n-2;i>=1;i--){
+            dp=Math.max(dp,pre[i]-dp);
         }
-
         return dp;
     }
 }
