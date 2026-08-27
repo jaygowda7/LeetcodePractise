fa78454f62c21c8cf1c48462dcca100264414e2a
Jayanth Gowda C <121957270+jaygowda7@users.noreply.github.com>
2026-08-27 21:02:43 +0530

    Time: 2 ms (41.89%), Space: 44.2 MB (29.05%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.java
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.java
@@ -1,34 +1,28 @@
 class Solution {
     public String shortestBeautifulSubstring(String s, int k) {
-        int n = s.length();
-        int minLen = Integer.MAX_VALUE;
+        int n=s.length();
+        int min=Integer.MAX_VALUE;
         String res = "";
-
-        // Iterate through all possible starting points
-        for (int i = 0; i < n; i++) {
-            int count = 0;
-            // Expand the window to the right
-            for (int j = i; j < n; j++) {
-                if (s.charAt(j) == '1') {
+        for(int i=0;i<n;i++){
+            int count=0;
+            for(int j=i;j<n;j++){
+                if(s.charAt(j)=='1'){
                     count++;
                 }
-                
-                // When we find exactly k ones
-                if (count == k) {
-                    int currLen = j - i + 1;
-                    String currStr = s.substring(i, j + 1);
-                    
-                    // Update if it's shorter, or lexicographically smaller if lengths match
-                    if (currLen < minLen) {
-                        minLen = currLen;
-                        res = currStr;
-                    } else if (currLen == minLen) {
-                        if (currStr.compareTo(res) < 0) {
-                            res = currStr;
+                if(count==k){
+                    int curlen=j-i+1;
+                    String curstr=s.substring(i,j+1);
+                    if(curlen<min){
+                        res=curstr;
+                        min=curlen;
+                    }else if(curlen==min){
+                        if(curstr.compareTo(res)<0){
+                            res=curstr;
                         }
                     }
-                    break; // Shortest possible substring starting at index i
+                    break;
                 }
+                
             }
         }
         return res;
