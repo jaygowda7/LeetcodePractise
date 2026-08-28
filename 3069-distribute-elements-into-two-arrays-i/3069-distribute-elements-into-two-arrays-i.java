75f992ac88fbeea8c28521fb1b352a96be221d8d
Jayanth Gowda C <121957270+jaygowda7@users.noreply.github.com>
2026-08-28 23:51:13 +0530

    Time: 2 ms (48.11%), Space: 46.6 MB (74.45%) - LeetHub

--- 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.java
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.java
@@ -1,24 +1,25 @@
 class Solution {
     public int[] resultArray(int[] nums) {
+         int n=nums.length;
+         List<Integer> n1=new ArrayList<>();
+         List<Integer> n2=new ArrayList<>();
+         n1.add(nums[0]);
+         n2.add(nums[1]);
+         for(int i=2;i<n;i++){
+            int x=n1.get(n1.size()-1);
+            int y=n2.get(n2.size()-1);
 
-        int n=nums.length;
-        List<Integer> arr1=new ArrayList<>();
-        List<Integer> arr2=new ArrayList<>();
-        arr1.add(nums[0]);
-        arr2.add(nums[1]);
-
-        for(int i=2;i<n;i++){
-            int num1=arr1.get(arr1.size()-1);
-            int num2=arr2.get(arr2.size()-1);
-
-            if(num1>num2) arr1.add(nums[i]);
-            else arr2.add(nums[i]);
-        }
-        int []res=new int[n];
-        int id=0;
-        for(int x:arr1) res[id++]=x;
-        for(int y:arr2) res[id++]=y;
-        
-        return res;
+            if(x>y){
+                n1.add(nums[i]);
+            }else{
+                n2.add(nums[i]);
+            }
+         }
+         int []res=new int[n];
+         int id=0;
+         for(int x:n1) res[id++]=x;
+         for(int y:n2) res[id++]=y;
+         return res;
     }
+    
 }
