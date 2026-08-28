a1a1f62bc9bfdd3ec21f09de7fa5649cb6b5e730
Jayanth Gowda C <121957270+jaygowda7@users.noreply.github.com>
2026-08-28 23:41:14 +0530

    Time: 4 ms (47.59%), Space: 92.2 MB (16.01%) - LeetHub

--- 1872-stone-game-viii/1872-stone-game-viii.java
+++ 1872-stone-game-viii/1872-stone-game-viii.java
@@ -8,7 +8,7 @@ class Solution {
         }
         int dp=pre[n-1];
         for(int i=n-2;i>=1;i--){
-            dp=Math.max(dp,dp-pre[i]);
+            dp=Math.max(dp,pre[i]-dp);
         }
         return dp;
     }
