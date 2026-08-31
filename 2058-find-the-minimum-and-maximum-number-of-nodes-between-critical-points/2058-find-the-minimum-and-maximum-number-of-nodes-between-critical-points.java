c0cdfd7e13548299771ea141cb676f9df9469a94
Jayanth Gowda C <121957270+jaygowda7@users.noreply.github.com>
2026-08-31 23:04:45 +0530

    Time: 6 ms (26.64%), Space: 106.4 MB (26.71%) - LeetHub

---
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int[] nodesBetweenCriticalPoints(ListNode head) {
+        int min=100000,i=1;
+        int first=0,last=0;
+
+        ListNode prev=head,cur=head.next,nxt=head.next.next;
+        while(nxt!=null){
+            if(isCrit(prev,cur,nxt)){
+                if(first==0) first=i;
+                else min=Math.min(min,i-last);
+                last=i;
+            }
+            prev=cur;cur=cur.next;
+            nxt=nxt.next;i++;
+        }
+        if(first==last) return new int[]{-1,-1};
+        return new int[]{min,last-first};
+        
+    }
+
+    boolean isCrit(ListNode a,ListNode b,ListNode c){
+        return ((b.val>a.val)&&(b.val>c.val))||((b.val<a.val)&&(b.val<c.val));
+    }
+}
