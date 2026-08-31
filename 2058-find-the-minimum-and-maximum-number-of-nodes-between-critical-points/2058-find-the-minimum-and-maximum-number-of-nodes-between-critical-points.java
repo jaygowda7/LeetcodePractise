7d2ac31c0e65fd963334dae4c892e6ab4c15fabb
Jayanth Gowda C <121957270+jaygowda7@users.noreply.github.com>
2026-08-31 23:16:52 +0530

    Time: 6 ms (26.64%), Space: 106.1 MB (55.32%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -11,24 +11,22 @@
 class Solution {
     public int[] nodesBetweenCriticalPoints(ListNode head) {
         int min=100000,i=1;
-        int first=0,last=0;
-
-        ListNode prev=head,cur=head.next,nxt=head.next.next;
+        int fi=0,la=0;
+        ListNode pre=head,cur=head.next,nxt=head.next.next;
         while(nxt!=null){
-            if(isCrit(prev,cur,nxt)){
-                if(first==0) first=i;
-                else min=Math.min(min,i-last);
-                last=i;
+            if(isCrit(pre,cur,nxt)){
+                if(fi==0) fi=i;
+                else min=Math.min(min,i-la);
+                la=i;
             }
-            prev=cur;cur=cur.next;
+            pre=cur;cur=cur.next;
             nxt=nxt.next;i++;
         }
-        if(first==last) return new int[]{-1,-1};
-        return new int[]{min,last-first};
+        if(fi==la) return new int[]{-1,-1};
+        return new int[]{min,la-fi};
         
     }
-
     boolean isCrit(ListNode a,ListNode b,ListNode c){
-        return ((b.val>a.val)&&(b.val>c.val))||((b.val<a.val)&&(b.val<c.val));
+        return (b.val>a.val&&b.val>c.val) || (b.val<a.val&&b.val<c.val);
     }
 }
